9643844645bd8d413c246482f6fd90c3ebb230a2
mazhihua <mazhihua@bftv.com>
2017-11-14 11:13:42 +0800

    UriScheme测试宿主工程，请勿下载

---
+++ app/src/main/java/fuck/knothing/com/mainurischema/MainActivity.kt
@@ -0,0 +1,14 @@
+package fuck.knothing.com.mainurischema
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"这是主APP", Toast.LENGTH_SHORT).show()
+    }
+}
